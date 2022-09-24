59f1af786c204998c37801ca57994ab4c2318878
mat3e <mateusz@brightcube.eu>
2022-09-24 21:48:28 +0200

    Simplify modules, update Spring

--- src/main/resources/db/migration/V2__init_categories_table.sql
+++ src/main/resources/db/migration/V2__init_categories_table.sql
@@ -1,6 +1,6 @@
 create table categories
 (
-    id            bigint(20) primary key auto_increment,
+    id            bigint primary key auto_increment,
     code          varchar(3)  not null,
     name          varchar(20) not null,
     description   varchar,
